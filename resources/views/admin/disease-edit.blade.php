d59d8e1e124658c39327c590a5363da638c44734
nhan123615 <64056052+nhan123615@users.noreply.github.com>
2020-10-16 09:39:40 +0700

    16.10.20

--- resources/views/admin/disease-edit.blade.php
+++ resources/views/admin/disease-edit.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.admin', ['pageHeader' => 'Disease Edit'])
 @section('head-section')
 
-<link rel="stylesheet" href="css/wmwysiwygeditor.css" type="text/css" media="screen" />
+<link rel="stylesheet" href="{{asset('trumbowyg/dist/ui/trumbowyg.min.css')}}">
 @endsection
 
 @section('content')
@@ -18,12 +18,7 @@
                         <div class="col-sm-3">
                             <label class="col-form-label">Name</label>
                         </div>
-                        <div class="col-sm-9">
-                            <div id="wmwysiwyg-basic-use"></div>
-                          {{--   <textarea id="tinymce-editor" class="trumbowyg-textarea" tabindex="-1" style="height: 42px;">
-                            </textarea> --}}
-              </textarea>
-
+                        <div class="col-sm-9">                                       
                             <input type="text" class="form-control mob_no" data-mask="9999-999-999" autocomplete="off" maxlength="12">
                         </div>
                     </div>
@@ -32,7 +27,9 @@
                             <label class="col-form-label">Description</label>
                         </div>
                         <div class="col-sm-9">
-                            <input type="text" class="form-control phone" data-mask="9999-9999" autocomplete="off" maxlength="9">
+                            <textarea class="editor">
+                                abc
+                            </textarea>
                         </div>
                     </div>
                     <div class="row form-group">
@@ -40,7 +37,8 @@
                             <label class="col-form-label">Statistics</label>
                         </div>
                         <div class="col-sm-9">
-                            <input type="text" class="form-control telphone_with_code" data-mask="(99) 9999-9999" autocomplete="off" maxlength="14">
+                            <textarea class="editor">
+                            </textarea>
                         </div>
                     </div>
                     <div class="row form-group">
@@ -48,7 +46,8 @@
                             <label class="col-form-label">Causes</label>
                         </div>
                         <div class="col-sm-9">
-                            <input type="text" class="form-control us_telephone" data-mask="(999) 999-9999" autocomplete="off" maxlength="14">
+                            <textarea class="editor">
+                            </textarea>
                         </div>
                     </div>
 
@@ -57,7 +56,8 @@
                             <label class="col-form-label">Symptoms</label>
                         </div>
                         <div class="col-sm-9">
-                            <input type="text" class="form-control us_telephone" data-mask="(999) 999-9999" autocomplete="off" maxlength="14">
+                            <textarea class="editor">
+                            </textarea>
                         </div>
                     </div>
 
@@ -67,7 +67,8 @@
                             <label class="col-form-label">Prevention</label>
                         </div>
                         <div class="col-sm-9">
-                            <input type="text" class="form-control us_telephone" data-mask="(999) 999-9999" autocomplete="off" maxlength="14">
+                            <textarea class="editor">
+                            </textarea>
                         </div>
                     </div>
                 </form>
@@ -80,27 +81,11 @@
 
 @section('footer-section')
 
-<script type="text/javascript" src="js/wmwysiwygeditor.js"></script>
+<script src="{{asset('trumbowyg/dist/trumbowyg.min.js')}}"></script>
 
 <script>
 $( document ).ready(function() {
-    $('#wmwysiwyg-with-options').wmwysiwyg({
-    toolbar_class: 'text-center bg-primary',
-    toolbar_btn_size: 'sm',
-    toolbar_btn_class: 'btn-primary',
-    height: 300,
-    btns: [
-        'fullScreen',
-        'formatting', 'fontSize',
-        'bold', 'italic', 'underline', 
-        'foreColor', 'backColor',
-        'link', 'insertImage', 'createTable',
-        'textAlignment',
-        'indent', 'outdent', 
-        'ul', 'ol', 'hr',
-        'removeFormat',
-    ],
-});
+    $('.editor').trumbowyg();
 });
 </script>
 @endsection
